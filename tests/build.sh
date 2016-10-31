daa272ded35c072ad375d59b825b5fa78e7fc286
Chris Houseknecht <chousekn@redhat.com>
2016-10-31 17:42:02 -0400

    Fix remote copy

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ansible-playbook build.yml | tee build.out
-grep failed=0 build.out >/dev/null
+grep failed=0 build.out | grep unreachable=0 >/dev/null
 
 if [ "$?" == "0" ]; then
     # if all looks good, give it a go
